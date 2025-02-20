094c58be5c7abdb717c2cefabbf5f962b8adfdef
Roslyn1203 <137019841+Roslyn1203@users.noreply.github.com>
2025-02-20 17:26:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 let buttonSize = 100;
 let combinedIndex = 1;  // 合并后的索引
-let buttonTexts = ["不可以", "我会洗香香的嘛", "求求你了～", "给你按摩可以嘛","100个亲亲呢","拜托","再不同意我就扑到宝宝","宝宝再考虑一下","最后一次机会了" ];  // 存储按钮2的内容
+let Texts = ["不可以",  "求求求求求求，求你了～", "我们不是全天下最最好的吗", "下次给宝宝按摩可以嘛","那现在买一杯奶茶呢","拜托了宝贝","买小蛋糕吃可以嘛，就原谅我了吧","那我们和好啦"];  // 存储按钮2的内容
 
+let button2Texts = ["不可以",  "不行", "不是", "不好","那我考虑考虑","你就会说这个","嗯"];  // 存储按钮2的内容
+
+let button1Texts = ["不可以",  "好吧", "是", "好～","那原谅你了","好吧好吧原谅你","那你还不快买","好" ];  // 存储按钮2的内容
 function changeAppearance() {
     let button1 = document.getElementById('button1');
     let button2 = document.getElementById('button2');
     let image = document.getElementById('dynamicImage');
-    
+    let dynamicText = document.getElementById('dynamicText');  // 获取 p 标签
     // 增加按钮1的尺寸
     buttonSize += 40;
     button1.style.width = `${buttonSize}px`;
@@ -15,26 +18,27 @@ function changeAppearance() {
     combinedIndex = (combinedIndex+1);
 
     // 改变按钮2的文字内容
-    button2.textContent = buttonTexts[combinedIndex-1];
+    dynamicText.textContent = Texts[combinedIndex-1];
 
-    if (combinedIndex === 11) {
+    button2.textContent = button2Texts[combinedIndex-1];
+    button1.textContent = button1Texts[combinedIndex-1];
+
+    let imageIndex = combinedIndex; // 根据 combinedIndex 来切换图片
+    image.src = `images/${imageIndex}.gif`;
+    if (combinedIndex === 8) {
         button2.remove(); // 删除按钮2
-    } else {
-        // 更新 combinedIndex，避免超出最大值
-	    // 切换图片
- 	   let imageIndex = combinedIndex; // 根据 combinedIndex 来切换图片
-	    image.src = `images/${imageIndex}.gif`;
-
-    }
+    } 
 }
 function changeAppearance2() {
     let button1 = document.getElementById('button1');
     let button2 = document.getElementById('button2');
     let image = document.getElementById('dynamicImage');
-    
+    let dynamicText = document.getElementById('dynamicText');  // 获取 p 标签
+	
+    dynamicText.textContent = "和你最好了宝宝"
     // 检查元素是否存在
     if (image) {
-        image.src = `images/99.gif`;
+        image.src = `images/66.gif`;
     }
 
     if (button2) {
